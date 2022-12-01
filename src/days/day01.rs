0ba6cdbb869cff672a0583ce9cebf0d7c086d014
Hyunwoong Kang <hyunwoong.kang@leedotype.com>
2022-12-01 14:05:54 +0900

    part 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,10 +1,27 @@
 use super::Solution;
 
-struct Day01 {}
+pub struct Day01 {}
+
+impl Day01 {
+    fn parse_input(input: String) -> Vec<i32> {
+        let mut ans = vec![];
+        let mut prev = 0;
+        for line in input.lines() {
+            if let Ok(num) = line.parse::<i32>() {
+                prev += num;
+            } else {
+                ans.push(prev);
+                prev = 0;
+            }
+        }
+        ans
+    }
+}
 
 impl Solution for Day01 {
     fn solve_part_1(input_file_name: &str) -> String {
-        unimplemented!("")
+        let nums = Self::parse_input(Self::read_input(input_file_name));
+        nums.iter().max().unwrap().to_string()
     }
     fn solve_part_2(input_file_name: &str) -> String {
         unimplemented!("")
