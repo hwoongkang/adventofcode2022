4694938d4feeb6bb68e2702c3956b2c681ac8944
Hyunwoong Kang <hyunwoong.kang@leedotype.com>
2022-12-03 14:12:31 +0900

    part 1

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,12 +1,81 @@
 use super::Solution;
+use std::collections::HashSet;
 
 pub struct Day03 {}
 
 impl Solution for Day03 {
     fn solve_part_1(input_file_name: &str) -> String {
-        unimplemented!("")
+        let input = Self::read_input(input_file_name);
+        Self::part_1(input)
     }
     fn solve_part_2(input_file_name: &str) -> String {
-        unimplemented!("")
+        String::from("0")
+    }
+}
+impl Day03 {
+    fn get_priority(c: char) -> i32 {
+        let a = 'a' as i32;
+        let z = 'z' as i32;
+        let big_a = 'A' as i32;
+        let num = c as i32;
+
+        if a <= num && num <= z {
+            num - a + 1
+        } else {
+            num - big_a + 27
+        }
+    }
+
+    fn get_duplicate(line: &str) -> HashSet<char> {
+        let mut duplicates = HashSet::new();
+        let l = line.len();
+        let first_half: HashSet<char> = line.chars().take(l / 2).collect();
+
+        for ch in line.chars().skip(l / 2) {
+            if first_half.contains(&ch) {
+                duplicates.insert(ch);
+            }
+        }
+
+        duplicates
+    }
+    fn part_1(input: String) -> String {
+        let mut ans = 0;
+        for line in input.lines() {
+            let duplicates = Self::get_duplicate(line);
+            for ch in duplicates {
+                ans += Self::get_priority(ch);
+            }
+        }
+        ans.to_string()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_priority() {
+        assert_eq!(Day03::get_priority('a'), 1);
+        assert_eq!(Day03::get_priority('z'), 26);
+        assert_eq!(Day03::get_priority('A'), 27);
+        assert_eq!(Day03::get_priority('Z'), 52);
+    }
+    #[test]
+    fn test_day03_part_1() {
+        let input = String::from(
+            "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw",
+        );
+        assert_eq!(Day03::part_1(input), "157");
+    }
+    #[test]
+    fn test_day03_part_2() {
+        assert_eq!(Day03::solve_part_2("input.txt"), "0");
     }
 }
