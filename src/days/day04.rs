c56357e52bdaa9807b7fd2805b2e2951a3edeabf
Hyunwoong Kang <hyunwoong.kang@leedotype.com>
2022-12-04 16:03:29 +0900

    구조

---
+++ src/days/day04.rs
@@ -0,0 +1,12 @@
+use super::Solution;
+
+pub struct Day04 {}
+
+impl Solution for Day04 {
+    fn solve_part_1(input_file_name: &str) -> String {
+        unimplemented!("")
+    }
+    fn solve_part_2(input_file_name: &str) -> String {
+        unimplemented!("")
+    }
+}
