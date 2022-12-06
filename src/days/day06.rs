b7cd2f8b36fde77de10c39f5ccd33123861f9d4c
Kang Hyunwoong <hyunwoong.kang@leedotype.com>
2022-12-06 14:01:39 +0900

    구조

---
+++ src/days/day06.rs
@@ -0,0 +1,26 @@
+use super::Solution;
+
+pub struct Day06;
+
+impl Solution for Day06 {
+    fn solve_part_1(input: String) -> String {
+        unimplemented!("")
+    }
+    fn solve_part_2(input: String) -> String {
+        unimplemented!("")
+    }
+}
+
+#[cfg(test)]
+mod day06_tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(Day06::solve_part_1("".to_string()), "".to_string());
+    }
+    #[test]
+    fn test_part_2() {
+        assert_eq!(Day06::solve_part_2("".to_string()), "".to_string());
+    }
+}
