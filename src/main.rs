b5bb028677c444f20fffcd3972bb20c9281e8dd5
Hyunwoong Kang <hyunwoong.kang@leedotype.com>
2022-12-07 18:58:44 +0900

    구조

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use days::*;
 
 fn main() {
     let input = read_input("input.txt");
-    println!("Part 1: {}", Day06::solve_part_1(input.clone()));
-    println!("Part 2: {}", Day06::solve_part_2(input));
+    println!("Part 1: {}", Day07::solve_part_1(input.clone()));
+    println!("Part 2: {}", Day07::solve_part_2(input));
 }
